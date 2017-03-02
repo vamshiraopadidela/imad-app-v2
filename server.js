a804dcc1e330df4852492a75fbe5c030480d3485
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-03 00:20:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-    res.send("you are on article one");
-});
-app.get('/article-two',function(req,res){
-    res.send("you are on article two");
-});
-app.get('/article-three',function(req,res){
-    res.send("you are on article three");
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
