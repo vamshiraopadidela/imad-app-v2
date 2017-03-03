6248fceb0dc66d8748110aeceee040db93367efa
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-04 00:18:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ button.onclick=function(){
   request.onreadystate=function(){
      if(request.readystate==XMLHttp.done){
          if(request.status==200){
-             var counter=request.responetext;
+             var counter=request.responeText;
              var span=document.getElementById("count");
              span.innerHTML=counter.toString();
              
@@ -14,7 +14,7 @@ button.onclick=function(){
              }
      } 
   };
-  request.open('Get','http://cloud.imad.hasura.io/counter');
+  request.open('Get','http://cloud.imad.hasura.io/counter',true);
   request.send(null);
   
 };
