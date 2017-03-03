ffa8934fb5a90330e2257e134906b2f99494a274
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-03 23:51:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,8 @@
-console.log('Loadedr!');
-var element=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    element.style.marginLeft=marginLeft+'px';
-}
-element.onclick=function(){
-  var interval=setInterval(moveRight,100);
+var counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+    
 };
