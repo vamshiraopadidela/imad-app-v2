dbe3459d60988b93477d2369b73f2fd224d89783
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-04 15:48:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 var names=[];
 app.get('/sub-name/:name',function(req,res){
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
     
