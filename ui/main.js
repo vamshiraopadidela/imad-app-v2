cf7cccd5c80fbaf08d3f720fb61ad3f9e11f8ad0
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-04 16:13:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,30 @@
 var counter=0;
 var button=document.getElementById("counter");
-button.onclick=function(){
+var submit=document.getElementById('sub-btn');
+submit.onclick=function(){
   var request=new XMLHttpRequest(); 
   request.onreadystatechange=function(){
      if(request.readyState==XMLHttpRequest.DONE){
          if(request.status==200){
-             var counter= request.responeText;
-             var span=document.getElementById("count");
-             span.innerHTML=counter;
-             
-             
-             
-             }
-     } 
-  };
-  request.open('GET','http://vamshiraopadidela.imad.hasura-app.io/counter',true);
-  request.send(null);
-  
-};
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-var submit=document.getElementById('sub-btn');
-submit.onclick=function(){
-  var names=['name1','name2','name3'];
+    
+             var names =request.responseText;
+             names=JSON.parse(names);
   var list='';
   for(var i=0;i<names.length;i++){
       list +='<li>'+ names[i] +'</li>';
   }
   var ul=document.getElementById('namelist');
   ul.innerHTML=list;
+             
+             
+             
+             }
+     } 
+  };
+  var nameInput=document.getElementById('name');
+var name=nameInput.value;
+
+  request.open('GET','http://vamshiraopadidela.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
 };
