e49480153c5c4cc0f9b5a89ee6d51974684de198
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-22 22:06:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     database:'vamshiraopadidela',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD,
+    password:'db-vamshiraopadidela-82102',
 };
 var app = express();
 app.use(morgan('combined'));
