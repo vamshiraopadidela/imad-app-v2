c118513e79b65741545e48effbe18168bc068b0b
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-27 23:13:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var config={
-    user:'coco98',
-    database:'coco98',
+    user:'vamshiraopadidela',
+    database:'vamshiraopadidela',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD,
