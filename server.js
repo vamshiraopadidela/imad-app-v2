b3fc906666d1a417cc5eb3fa637ba6a4cbc91f36
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-28 01:13:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,22 @@ app.get('/test-db',function(req,res){
         }
     });
 });
-
+app.get('/articles/:articleName', function (req, res) {
+pool.query("SELECT * FROM article WHERE title= '"+req.params.articleName+ "'", function(err,result){
+if (err){
+res.status(500).send(err.toString());
+}
+else{
+if(result.rows.length===0){
+res.status(404).send('Article not found');
+}
+else{
+var articleData= result.rows[0];
+res.send(createTemplate(articles[articleName]));
+}
+}
+})
+})
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
