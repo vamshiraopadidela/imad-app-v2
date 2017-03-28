bc693c1f0a59fa87c0ef2ab6dc47d01d24b58cf4
vamshiraopadidela <vamshiraopadidela@gmail.com>
2017-03-28 18:21:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/ui/main.js', function (req, res) {
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     
-    pool.query('SELECT * FROM test',function(err,result){
+    pool.query('SELECT * FROM save',function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
